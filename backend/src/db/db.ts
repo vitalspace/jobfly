6384cdfd30f805577b53f27c82dafa33fca16dd0
vitalspace <myglobalsounds@gmail.com>
2024-07-11 17:06:37 -0600

    db connection

---
+++ backend/src/db/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const connectDb = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+        )
+        console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MongoDb connection FAILED: ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDb;
