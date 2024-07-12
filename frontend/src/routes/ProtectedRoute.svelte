395710d7710abe6a2f49ba8c70641e0984cf77bd
vitalspace <myglobalsounds@gmail.com>
2024-07-11 20:07:40 -0600

    added protected route manager

---
+++ frontend/src/routes/ProtectedRoute.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { navigate } from "svelte-routing";
+  import { auth } from "../stores/auth.store";
+  import { onMount } from "svelte";
+
+  export let component: any;
+  let isAuthenticated: boolean = false;
+
+  onMount(() => {
+    const unsubscribe = auth.subscribe((value) => {
+      isAuthenticated = value;
+      if (!isAuthenticated) {
+        navigate("/signin");
+      }
+    });
+
+    return unsubscribe;
+  });
+</script>
+
+{#if isAuthenticated}
+  <svelte:component this={component} />
+{/if}
