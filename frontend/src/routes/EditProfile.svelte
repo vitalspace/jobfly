52015c53acf991f2741ce3dff8b5fab8d3015752
vitalspace <myglobalsounds@gmail.com>
2024-07-12 23:22:11 -0600

    added editprofile router

---
+++ frontend/src/routes/EditProfile.svelte
@@ -0,0 +1,55 @@
+<div class="grid gap-6 p-4">
+    <div class="grid gap-y-6 text-center">
+      <div>
+        <h2 class="prompt-semibold text-sm">Fill out your profile</h2>
+      </div>
+      <div class="grid place-items-center gap-2">
+        <div class="flex place-items-end">
+          <div class="rounded-full border-2 p-2">
+            <svg class="h-20 w-20 text-slate-600" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" />
+              <circle cx="12" cy="7" r="4" />
+              <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            </svg>
+          </div>
+          <div class="absolute cursor-pointer pl-16">
+            <div class="rounded-full border border-white bg-[#4968d7]">
+              <svg class="h-6 w-6 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <line x1="12" y1="5" x2="12" y2="19" />
+                <line x1="5" y1="12" x2="19" y2="12" />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <p class="prompt-extralight text-xs">Upload a photo</p>
+      </div>
+    </div>
+  
+    <div>
+      <form action="" class="flex flex-col gap-y-2">
+        <label for="" class="prompt-extralight text-xs">Full name</label>
+        <input class="prompt-extralight rounded-2xl border py-2 pl-2" type="text" />
+  
+        <label for="" class="prompt-extralight text-xs">Age</label>
+        <input class="prompt-extralight rounded-2xl border py-2 pl-2" type="text" />
+  
+        <label for="" class="prompt-extralight text-xs">Location</label>
+        <input class="prompt-extralight rounded-2xl border py-2 pl-2" type="text" />
+  
+        <label for="" class="prompt-extralight text-xs">Phone</label>
+        <input class="prompt-extralight rounded-2xl border py-2 pl-2" type="text" />
+  
+        <label for="" class="prompt-extralight text-xs">Email</label>
+        <input class="prompt-extralight rounded-2xl border py-2 pl-2" type="text" />
+  
+        <label for="" class="prompt-extralight text-xs">Password</label>
+        <input class="prompt-extralight rounded-2xl border py-2 pl-2" type="text" />
+      </form>
+    </div>
+  
+    <div class="">
+      <button class="prompt-semibold w-full rounded-2xl bg-black py-3 text-sm text-white">Submit</button>
+    </div>
+  </div>
+  
