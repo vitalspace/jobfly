f3731c54947adcb5f8ee86e036eadc7c380b23db
vitalspace <myglobalsounds@gmail.com>
2024-07-13 01:48:16 -0600

    added more services

--- frontend/src/services/user.services.ts
+++ frontend/src/services/user.services.ts
@@ -1,3 +1,6 @@
 import axios from "./axios";
+import { type UserData} from "../types/types.d";
 
+export const signUp = async (body: UserData) => await axios.post("/signup", body);
+export const signIn = async (body: Partial<UserData>) => await axios.post("/signin", body);
 export const getProfile = async () => axios.get("/profile");
