194506b5edae1b93b99e072717fed9efb76cb609
vitalspace <myglobalsounds@gmail.com>
2024-07-14 22:07:56 -0600

    updated base url

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,10 +1,13 @@
 import axios, { type AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://192.168.56.1:3000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
+  // headers: {
+  //   Accept: "application/json, text/plain, */*",
+  // },
   withCredentials: true,
 });
 
