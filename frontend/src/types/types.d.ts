b2f849a2a9b514309dda255c9a3a2f840889399a
vitalspace <myglobalsounds@gmail.com>
2024-07-14 22:07:17 -0600

    updated types

--- frontend/src/types/types.d.ts
+++ frontend/src/types/types.d.ts
@@ -1,7 +1,14 @@
-export interface UserData {
+export enum Role {
+  Recruiter,
+  Candidate
+}
+
+export interface IUser {
+  avatar: string;
   fullname: string;
   age: number | null;
   location: string;
+  rol: Rol;
   phone: string;
   email: string;
   password: string;
