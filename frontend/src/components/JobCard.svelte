36d5db1163652a29fd17b2d5a0b585d782c927cd
vitalspace <myglobalsounds@gmail.com>
2024-07-21 23:01:24 -0600

    added job card

---
+++ frontend/src/components/JobCard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { BuildingSolid, BookmarkSolid, DotsVerticalOutline } from "flowbite-svelte-icons";
+  import { Link } from "svelte-routing";
+
+    // JobCard component
+    export let job;
+    export let onUnsave: () => void = () => {};
+    export let onShowModal: () => void = () => {};
+  </script>
+  
+  <div class="border flex justify-between place-items-center">
+    <div class="flex place-items-center space-x-2">
+      <div>
+        <BuildingSolid color="#C5C7CC" size="lg" />
+      </div>
+      <div>
+        <Link to={`/job/${job._id}`} class="prompt-semibold text-md">{job.title}</Link>
+        <p class="prompt-extralight text-xs">{job.company}</p>
+        <p class="prompt-extralight text-xs">{job.location}</p>
+      </div>
+    </div>
+    <div class="pr-2">
+      {#if onUnsave}
+        <button on:click={onUnsave}>
+          <BookmarkSolid size="lg" color="#F50049" />
+        </button>
+      {:else}
+        <button on:click={onShowModal}>
+          <DotsVerticalOutline />
+        </button>
+      {/if}
+    </div>
+  </div>
