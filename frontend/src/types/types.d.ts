0460d7472f6a242b869789ba8df66cae0a157b16
vitalspace <myglobalsounds@gmail.com>
2024-07-21 23:06:51 -0600

    update types

--- frontend/src/types/types.d.ts
+++ frontend/src/types/types.d.ts
@@ -1,9 +1,10 @@
 export enum Role {
   Recruiter,
-  Candidate
+  Candidate,
 }
 
 export interface IUser {
+  _id: string;
   avatar: string;
   fullname: string;
   age: number | null;
@@ -12,4 +13,32 @@ export interface IUser {
   phone: string;
   email: string;
   password: string;
+  applications: [] | undefined;
+  savedJobs: [] | undefined;
+}
+
+export interface IJob {
+  // _id: string;
+  // title: string;
+  // company: string;
+  // employment: string;
+  // location: string;
+  // salary: number;
+  // description: string;
+  // inthisrole: string;
+  // skills: string;
+  // level: string;
+  _id: string;
+  avatar: string;
+  title: string;
+  company: string;
+  employment: string;
+  location: string;
+  salary: number;
+  description: string;
+  inthisrole: string;
+  skills: string;
+  level: string;
+  recruiter: string;
+  applicants: string[];
 }
