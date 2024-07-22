47d63d4d7a60c3a44e618bd1f7554cecdfd09920
vitalspace <myglobalsounds@gmail.com>
2024-07-21 23:06:28 -0600

    updated user service

--- frontend/src/services/user.services.ts
+++ frontend/src/services/user.services.ts
@@ -1,7 +1,19 @@
 import axios from "./axios";
-import { type IUser} from "../types/types.d";
+import { type IUser } from "../types/types.d";
 
-export const signUp = async (body: IUser) => await axios.post("/signup", body);
-export const signIn = async (body: Partial<IUser>) => await axios.post("/signin", body);
-export const update = async (body: Partial<IUser>) => await axios.put("/update", body);
+export const signUp = async (body: Partial<IUser>) =>
+  await axios.post("/signup", body);
+export const signIn = async (body: Partial<IUser>) =>
+  await axios.post("/signin", body);
+export const update = async (body: Partial<IUser>) =>
+  await axios.put("/update", body);
 export const getProfile = async () => axios.get("/profile");
+export const postulate = async (body: { id: string }) =>
+  axios.post(`/apply`, body);
+export const unApply = async (body: { id: string }) =>
+  axios.post(`/unapply`, body);
+export const saveJob = async (body: { id: string }) =>
+  axios.post(`/savejob`, body);
+export const unSavedJob = async (body: { id: string }) =>
+  axios.post(`/unsavedjob`, body);
+export const getMyJobs = async () => await axios.get(`/getmyjobs`);
